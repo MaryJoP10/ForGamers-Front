8eb0afab044624c2bd583b69041289c9392de018
Shana <maryjop10@gmail.com>
2022-05-15 14:20:09 -0500

    Removing react icons

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -3,7 +3,6 @@ import "./navbar.css";
 import { NavLink as Link } from "react-router-dom";
 import styled from 'styled-components'
 import logo from '../../images/logo.png';
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -38,7 +37,7 @@ const Navbar = () => {
         </div>
 
         <div className="responsive">
-          {toggleMenu
+          {/**toggleMenu
             ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
             : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
           {toggleMenu && (
@@ -63,7 +62,7 @@ const Navbar = () => {
                 </NavLink>
               </div>
             </div>
-          )}
+          )*/}
         </div>
       </Nav>
     </>
